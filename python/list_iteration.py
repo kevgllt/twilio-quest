9e3f7913da38c147561e20c4c9494b3901051f92
kevgllt <kevingullytps@gmail.com>
2023-02-20 20:06:16 -0300

    Solved Trial of Iteration

--- python/list_iteration.py
+++ python/list_iteration.py
@@ -1,6 +1,16 @@
-groceries = ['apples', 'coffee', 'pizza rolls', 'olives']
+import sys
 
-print("These are the items on my grocery list:")
-for index, item in enumerate(groceries, start=1):
+# groceries = ['apples', 'coffee', 'pizza rolls', 'olives']
+
+#print("These are the items on my grocery list:")#
+# for index, item in enumerate(groceries, start=1):
+#     string_to_print = f"{index}. {item}"
+#     print(string_to_print)
+
+
+order_of_succession = sys.argv
+order_of_succession.pop(0)
+
+for index, item in enumerate(order_of_succession, start=1):
     string_to_print = f"{index}. {item}"
     print(string_to_print)
